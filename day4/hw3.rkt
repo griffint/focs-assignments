12bd45e130749783caf6973ea2513918e2c0e0ca
Griffin Tschurwald <griffin.tschurwald@gmail.com>
2016-09-15 03:04:48 -0400

    homework 4 is done

--- day4/hw3.rkt
+++ day4/hw3.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Griffin Tschurwald [change to your name]
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [x] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
@@ -17,9 +17,18 @@
 ;;
 ;; If the key is not found in the list, `assq` returns `#f`.
 
+(define (my-assq key ass-list)
+  (if (null? ass-list)
+      #f
+      (if (eq? key (first (first ass-list)))
+          (first ass-list)
+          (my-assq key (rest ass-list))
+          )
+      )
+  )
 
-
-
+;; Test to see if it works, should return "(cat 3)"
+(display(assq 'cat '((dog 1) (horse 2) (cat 3))))
 
 ;;;;;;;;;;;
 ;; 2. lookup-list
@@ -28,3 +37,33 @@
 ;;
 ;; Add the `lookup-list` argument to your hw2 evaluator (or ours, from the solution set).
 ;; `(evaluate 'foo lookup-list)` should return whatever `'foo` is associated with in `lookup-list`.
+
+(define (calc-find-symbol y)
+  (cond ((eq? 'ADD y) +)
+        ((eq? 'SUB y) -)
+        ((eq? 'MUL y) *)
+        ((eq? 'DIV y) /)
+        ((eq? 'GT y) >)
+        ((eq? 'LT y) <)
+        ((eq? 'GE y) >=)
+        ((eq? 'LE y) <=)
+        ((eq? 'EQ y) =)
+        ((eq? 'NEQ y) (lambda (x y) (not (eq? x y)))) ;;lambda functions for these, no built in symbols that i know of
+        ((eq? 'AND y) (lambda (x y) (and x y)))
+        ((eq? 'NOR y) (lambda (x y) (nor x y)))
+        ((eq? 'NOT y) (lambda (x y) (not x y)))
+        )
+  )
+            
+(define (calculate x)
+  (if (number? x) ;;if x is a number just return it, this is for recursion
+      x
+      (if (eq? (first x) 'IPH) ;; check if first symbol is IPH for special case
+          (if (calculate (second x))
+              (calculate (third x))
+              (calculate (fourth x))
+              )
+          ((calc-find-symbol (first x)) (calculate (second x)) (calculate (third x)))
+          )
+  )
+ )
